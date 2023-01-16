1c0e64fe2f487480d814f2bea4eb24041b88f127
SeongHeon Sim <river.water.sea@gmail.com>
2023-01-16 23:52:12 +0900

    #2 Init Project

---
+++ kotlin-clean-architecture/src/main/kotlin/com/study/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package com.study.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCleanArchitectureApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCleanArchitectureApplication>(*args)
+}
