b87633d4bacefc0125a80af7f2f171e2e5dcc4f4
SeongHeon Sim <river.water.sea@gmail.com>
2023-02-13 23:46:45 +0900

    #1 모든 유저 조회 테스트 추가

--- kotlin-spring-boot/module-api/src/test/kotlin/com/study/kotlin/user/application/service/UserServiceTest.kt
+++ kotlin-spring-boot/module-api/src/test/kotlin/com/study/kotlin/user/application/service/UserServiceTest.kt
@@ -1,13 +1,21 @@
 package com.study.kotlin.user.application.service
 
 import com.study.kotlin.extension.age
+import com.study.kotlin.extension.id
 import com.study.kotlin.extension.name
 import com.study.kotlin.user.application.command.input.RegisterUserCommand
 import com.study.kotlin.user.application.port.input.RegisterUserPort
 import com.study.kotlin.user.application.port.input.UpdateUserPort
 import com.study.kotlin.user.application.port.output.GetUserPort
+import com.study.kotlin.user.domain.User
 import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
+import io.kotest.property.RandomSource
+import io.kotest.property.arbitrary.chunked
+import io.kotest.property.arbitrary.of
+import io.kotest.property.arbitrary.single
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
@@ -34,6 +42,22 @@ class UserServiceTest : ExpectSpec({
 
             verify(exactly = 1) { registerUserPort.registerUser(any()) }
             verify(exactly = 1) { userService.registerUser(any()) }
+        }
+
+        expect("모든 유저 조회") {
+            val user = Arb.of(
+                User(
+                    id = Arb.id(),
+                    name = Arb.name(),
+                    age = Arb.age()
+                )
+            )
+            every { userService.getAllUser() } returns user.chunked(10..10)
+                .single(RandomSource.default())
+
+            val userList = userService.getAllUser()
+
+            userList.size shouldBe 10
         }
     }
 })
